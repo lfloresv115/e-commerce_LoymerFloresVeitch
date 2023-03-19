691784aa508d50bc23ee9fb14e5851f0f7188db3
lfloresv115 <128271133+lfloresv115@users.noreply.github.com>
2023-03-18 21:10:25 -0600

    Add files via upload

---
+++ EcommerceLoymer/EcommerceLoymer/assets/js/data/data.js
@@ -0,0 +1,67 @@
+const db = [
+  {
+    id: 1,
+    nombre: 'Closet',
+    descripcion: 'Closed of polished nogal',
+    precio: 2000.00,
+    imagen: './assets/img/closet.jpg',
+    categoria: 'Bedroom',
+    cantidad: 5
+  },
+  {
+    id: 2,
+    nombre: 'Table',
+    descripcion: 'Rustic wood dining table',
+    precio: 500.00,
+    imagen: './assets/img/table.jpg',
+    categoria: 'Dining room',
+    cantidad: 10
+  },
+  {
+    id: 3,
+    nombre: 'Door',
+    descripcion: 'Door made to measure, with very long-lasting oak wood',
+    precio: 800.00,
+    imagen: './assets/img/doors.jpeg',
+    categoria: 'Outdoor',
+    cantidad: 2
+  },
+  {
+    id: 4,
+    nombre: 'Living room furniture',
+    descripcion: 'A very good living room furniture premium',
+    precio: 1500.00,
+    imagen: './assets/img/living room furniture.jpg',
+    categoria: 'Living Room',
+    cantidad: 20
+  },
+  {
+    id: 5,
+    nombre: 'Decor',
+    descripcion: 'A beautiful wooden sculpture.',
+    precio: 120.00,
+    imagen: './assets/img/decor.jpeg',
+    categoria: 'Wood crafts',
+    cantidad: 50
+  },
+  {
+    id: 6,
+    nombre: 'cot',
+    descripcion: 'Baby cribs made of ebony wood.',
+    precio: 1000.00,
+    imagen: 'assets/img/cot.jpg',
+    categoria: 'children room',
+    cantidad: 10
+  },
+  {
+    id: 7,
+    nombre: 'Office furniture',
+    descripcion: 'Beautiful office furniture with teak wood',
+    precio: 5000.00,
+    imagen: './assets/img/office.jpg',
+    categoria: 'Office',
+    cantidad: 1
+  }
+]
+
+export default db
